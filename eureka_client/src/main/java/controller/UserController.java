fbd105c462c89fa745c5ec47c356d1895e925a5e
leaf222 <ahtlye@163.com>
2020-01-04 16:29:13 +0800

    UserController

--- eureka_client/src/main/java/controller/UserController.java
+++ eureka_client/src/main/java/controller/UserController.java
@@ -1,10 +1,30 @@
 package controller;
 
+import com.example.UserAccess.UserSign;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @program: appointment
  * @description:
  * @author: Yifan Ye
  * @create: 2020/01/04
  **/
-public class UserController {
+@RestController
+@RequestMapping(value = "/user")
+public class UserController
+{
+    @Autowired
+    UserSign userSign;
+
+    @PostMapping("/signin")
+    public JSONObject ControllerSignIn(@RequestBody JSONObject jsonObject)
+    {
+        return  userSign.SignIn(jsonObject.getString("account")
+        ,jsonObject.getString("password"));
+    }
 }
